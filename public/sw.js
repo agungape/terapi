a19519f9e508f103e8921cdd9bf000e288fde5b1
agungape <muh.agungdwijuniawan@gmail.com>
2025-07-31 00:04:55 +0800

    update ke -93

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,14 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installed.');
-});
+const CACHE_NAME = 'bsc-v3';
+const urlsToCache = [
+  '/mobile/',
+  '/mobile/dashboard',
+  '/offline' // Buat route fallback
+];
 
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request)
+      .catch(() => caches.match('/mobile/'))
+  ));
+});

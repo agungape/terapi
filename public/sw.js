51025964cc649af92ef01b49c461810fe20d6b2a
agungape <muh.agungdwijuniawan@gmail.com>
2025-07-31 08:47:58 +0800

    update ke -93

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,14 @@
 const CACHE_NAME = 'bsc-v3';
 const urlsToCache = [
-  '/mobile/',
-  '/mobile/dashboard',
-  '/offline' // Buat route fallback
+  'brightchild.id/mobile/',
+  'brightchild.id/mobile/dashboard',
+  'brightchild.id/offline' // Buat route fallback
 ];
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request)
-      .catch(() => caches.match('/mobile/'))
+      .catch(() => caches.match('brightchild.id/mobile/'))
   ));
 });

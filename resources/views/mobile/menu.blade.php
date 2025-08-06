4a1c8756d51964a8705c2dbee2c3bc3d94098f3f
agungape <muh.agungdwijuniawan@gmail.com>
2025-08-06 11:12:32 +0800

    update ke -105

--- resources/views/mobile/menu.blade.php
+++ resources/views/mobile/menu.blade.php
@@ -1,5 +1,5 @@
 <div class="sidebar">
-    <a href="profile.html" class="author-box">
+    <a class="author-box">
         <div class="dz-media">
             <img src="{{ $anak->foto ? asset('storage/anak/' . $anak->foto) : asset('assets/mobile/pixio/images/foto-anak/small.png') }}"
                 alt="author-image">

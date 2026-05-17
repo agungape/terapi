70075d8ac25aacd1417221854be9f1e1d8e181a9
agungape <muh.agungdwijuniawan@gmail.com>
2026-05-17 15:00:02 +0800

    remove notification bell icon from main page header

--- resources/views/mobile-new/partials/header.blade.php
+++ resources/views/mobile-new/partials/header.blade.php
@@ -43,15 +43,6 @@
                     <h1 class="text-xl font-bold">{{ $anak->nama ?? 'Nama Anak' }}</h1>
                 </div>
             </div>
-            <button @click="markNotificationRead" class="relative group ripple">
-                <div
-                    class="bg-white/20 p-3 rounded-2xl group-hover:bg-white/30 transition-all duration-300 group-active:scale-90">
-                    <i class="fa-regular fa-bell text-xl"></i>
-                    <span x-show="notificationCount > 0" x-text="notificationCount"
-                        class="absolute -top-1 -right-1 min-w-5 h-5 bg-yellow-400 text-indigo-900 text-xs font-bold rounded-full border-2 border-indigo-600 flex items-center justify-center animate-pulse-slow">
-                    </span>
-                </div>
-            </button>
         </div>
     </template>
 
